74aac598a97a3cbd9837b1bdc1a6c9584185bf7f
Iwan Rahman <1w3n.rahman@gmail.com>
2024-07-22 13:54:30 -0400

    Refresh Port Connection
    Disable Disconnect button upon start
    Disable connect button when connected
    Enable disconnect button when connected
    Disable disconnect button when disconnected
    Enable connect button when disconnected
    Refresh does not override connected input fields

--- main.py
+++ main.py
@@ -3,13 +3,12 @@ from PyQt5 import QtWidgets as qw, uic
 from MainWindow import Ui_MainWindow
 from ComManager import Ui_ComManager
 from graph import Ui_Graph
-from serialConnection import *
+from serialConnection import serial_connection
 
 class ComWindow(qw.QDialog,Ui_ComManager):
   def __init__(self):
     super().__init__()
     self.setupUi(self)
-    self.refresh()
     self.refreshBtn = qw.QPushButton("Refresh")
     self.refreshBtn.setObjectName("Refresh")
     self.refreshBtn.clicked.connect(self.refresh)
@@ -20,57 +19,85 @@ class ComWindow(qw.QDialog,Ui_ComManager):
     self.DisconnectBtnP2.clicked.connect(self.disconnectPort2)
     self.ConnectBtnP3.clicked.connect(self.connectPort3)
     self.DisconnectBtnP3.clicked.connect(self.disconnectPort3)
-    self.connectedPorts = [None, None, None]
+    self.initValues()
+    self.refresh()
 
   def refresh(self):
-    self.ports = get_ports()
+    self.ports = serial_connection.get_ports()
     print(self.ports)
-    self.PortInputP1.clear()
-    self.PortInputP2.clear()
-    self.PortInputP3.clear()
-    if(len(self.ports) == 0):
-      self.PortInputP1.insertItem(0,'No Ports Found')
-      self.PortInputP2.insertItem(0,'No Ports Found')
-      self.PortInputP3.insertItem(0,'No Ports Found')
-      self.PortInputP1.setEnabled(False)
-      self.PortInputP2.setEnabled(False)
-      self.PortInputP3.setEnabled(False)
-    else:
-      self.PortInputP1.setEnabled(True)
-      self.PortInputP2.setEnabled(True)
-      self.PortInputP3.setEnabled(True)
-      for x in self.ports:
-        self.PortInputP1.addItem(x)
-        self.PortInputP2.addItem(x)
-        self.PortInputP3.addItem(x)   
-      
+
+    # Reset Port 1 Input
+    if serial_connection.connectedPorts[0] == None:
+      self.PortInputP1.clear()
+      if(len(self.ports) == 0):
+        self.PortInputP1.insertItem(0,'No Ports Found')
+        self.PortInputP1.setEnabled(False)
+      else:
+        self.PortInputP1.setEnabled(True)
+        for x in self.ports:
+          self.PortInputP1.addItem(x)
+
+    # Reset Port 2 Input  
+    if serial_connection.connectedPorts[1] == None:
+      self.PortInputP2.clear()
+      if(len(self.ports) == 0):
+       self.PortInputP2.insertItem(0,'No Ports Found')
+       self.PortInputP2.setEnabled(False)
+      else:
+        self.PortInputP2.setEnabled(True)
+        for x in self.ports:
+          self.PortInputP2.addItem(x)
+    
+    # Reset Port 3 Input 
+    if serial_connection.connectedPorts[2] == None:
+      self.PortInputP3.clear()
+      if(len(self.ports) == 0):
+        self.PortInputP3.insertItem(0,'No Ports Found')
+        self.PortInputP3.setEnabled(False)
+      else:
+        self.PortInputP3.setEnabled(True)
+        for x in self.ports:
+          self.PortInputP3.addItem(x)   
+  
+  def initValues(self):
+    self.DisconnectBtnP1.setEnabled(False)
+    self.DisconnectBtnP2.setEnabled(False)
+    self.DisconnectBtnP3.setEnabled(False)
+
   def connectPort1(self):
     self.StopBitInput = None
     for radioBtn in self.StopBitInputP1.findChildren(qw.QRadioButton):
       if radioBtn.isChecked():
         self.StopBitInput = radioBtn
         break
-    
     try:
-      connectCOM(self,0,str(self.PortInputP1.currentText()), int(self.BaudInputP1.currentText()),int(self.ByteSizeInputP1.value()),
-               str(self.ParityInputP1.currentText()),int(self.StopBitInput.text()),int(self.TimeoutInputP1.value()))
+      serial_connection.connectCOM(
+        0,
+        str(self.PortInputP1.currentText()),
+        int(self.BaudInputP1.currentText()),
+        int(self.ByteSizeInputP1.value()),
+        str(self.ParityInputP1.currentText()),
+        int(self.StopBitInput.text()),
+        int(self.TimeoutInputP1.value())
+        )
       self.PortInputP1.setEnabled(False)
       self.BaudInputP1.setEnabled(False)
       self.ByteSizeInputP1.setEnabled(False)
       self.ParityInputP1.setEnabled(False)
       self.StopBitInputP1.setEnabled(False)
       self.TimeoutInputP1.setEnabled(False)
-    except:
-      reply = qw.QMessageBox.critical(self, 'Connection Error', 'There was an error connecting to the port',
-        qw.QMessageBox.Ok)
+      self.ConnectBtnP1.setEnabled(False)
+      self.DisconnectBtnP1.setEnabled(True)
+    except Exception as e:
+            reply = qw.QMessageBox.critical(
+                self, 
+                'Connection Error', 
+                f'There was an error connecting to the port: {e}',
+                qw.QMessageBox.Ok
+            )
 
-    
   def disconnectPort1(self):
-    if self.connectedPorts[0] != None:
-      try:
-        DisconnectCOM(self.connectedPorts[0])
-      except:
-        print('Error')
+    serial_connection.disconnectCOM(0)
     if(len(self.ports) != 0):
           self.PortInputP1.setEnabled(True)
     self.BaudInputP1.setEnabled(True)
@@ -78,6 +105,8 @@ class ComWindow(qw.QDialog,Ui_ComManager):
     self.ParityInputP1.setEnabled(True)
     self.StopBitInputP1.setEnabled(True)
     self.TimeoutInputP1.setEnabled(True)
+    self.ConnectBtnP1.setEnabled(True)
+    self.DisconnectBtnP1.setEnabled(False)
 
   def connectPort2(self):
     self.PortInputP2.setEnabled(False)
@@ -86,6 +115,8 @@ class ComWindow(qw.QDialog,Ui_ComManager):
     self.ParityInputP2.setEnabled(False)
     self.StopBitInputP2.setEnabled(False)
     self.TimeoutInputP2.setEnabled(False)
+    self.ConnectBtnP2.setEnabled(False)
+    self.DisconnectBtnP2.setEnabled(True)
   
   def disconnectPort2(self):
     if(len(self.ports) != 0):
@@ -95,6 +126,8 @@ class ComWindow(qw.QDialog,Ui_ComManager):
     self.ParityInputP2.setEnabled(True)
     self.StopBitInputP2.setEnabled(True)
     self.TimeoutInputP2.setEnabled(True)  
+    self.ConnectBtnP2.setEnabled(True)
+    self.DisconnectBtnP2.setEnabled(False)
 
   def connectPort3(self):
     if(len(self.ports) != 0):
@@ -104,6 +137,8 @@ class ComWindow(qw.QDialog,Ui_ComManager):
     self.ParityInputP3.setEnabled(False)
     self.StopBitInputP3.setEnabled(False)
     self.TimeoutInputP3.setEnabled(False)
+    self.ConnectBtnP3.setEnabled(False)
+    self.DisconnectBtnP3.setEnabled(True)
   
   def disconnectPort3(self):  
     self.PortInputP3.setEnabled(True)
@@ -112,7 +147,8 @@ class ComWindow(qw.QDialog,Ui_ComManager):
     self.ParityInputP3.setEnabled(True)
     self.StopBitInputP3.setEnabled(True)
     self.TimeoutInputP3.setEnabled(True)
-
+    self.ConnectBtnP3.setEnabled(True)
+    self.DisconnectBtnP3.setEnabled(False)
 
 
 class MainWindow(qw.QMainWindow, Ui_MainWindow):
--- serialConnection.py
+++ serialConnection.py
@@ -1,60 +1,79 @@
 import serial
 import serial.tools.list_ports
 
-def get_ports():
+class SerialConnection:
+    def __init__(self):
+        self.connectedPorts = {0: None, 1: None, 2: None}
 
-    ports = serial.tools.list_ports.comports()
-    ports = [com[0] for com in ports]
-    return ports
+    def get_ports(self):
+        ports = serial.tools.list_ports.comports()
+        ports = [com[0] for com in ports]
+        return ports
 
+    def connectCOM(self, portNo, com, baud, data, parity, stop, timeout):
+        serialByteSize = self.getSerialByteSize(data)
+        serialParity = self.getSerialParity(parity)
+        serialStopBits = self.getSerialStopBit(stop)
+        print(str(serialByteSize))
+        print(str(serialParity))
+        print(str(serialStopBits))
+        print(portNo)
+        print(self.connectedPorts[portNo])
+        if self.connectedPorts[portNo] is not None:
+            print("Port already connected")
+            return
+        self.connectedPorts[portNo] = serial.Serial(
+            com,
+            baudrate=baud,
+            bytesize=serialByteSize,
+            parity=serialParity,
+            stopbits=serialStopBits,
+            timeout=timeout
+        )
 
-def connectCOM(self, portNo, com, baud, data, parity, stop, timeout):
-    serialByteSize = getSerialByteSize(data)
-    serialParity = getSerialParity(parity)
-    serialStopBits = getSerialStopBit(stop)
-    print(str(serialByteSize))
-    print(str(serialParity))
-    print(str(serialStopBits))
-    print(portNo)
-    print(self.connectedPorts[portNo])
-    self.connectedPorts[portNo] = serial.Serial(com, baudrate = baud,bytesize=serialByteSize,parity=serialParity,stopbits=serialStopBits, timeout=timeout)
+    def disconnectCOM(self, portNo):
+        if self.connectedPorts[portNo] is not None:
+            self.connectedPorts[portNo].close()
+            self.connectedPorts[portNo] = None
+        else:
+            print(f"No connection found on port {portNo}")
 
-def DisconnectCOM(port):
-    port.close()
+    def getSerialByteSize(self, byteSizeInput):
+        byteSize = None
+        match byteSizeInput:
+            case 5:
+                byteSize = serial.FIVEBITS
+            case 6:
+                byteSize = serial.SIXBITS
+            case 7:
+                byteSize = serial.SEVENBITS
+            case 8:
+                byteSize = serial.EIGHTBITS
+        return byteSize
 
-def getSerialByteSize(byteSizeInput):
-    byteSize = None
-    match byteSizeInput:
-        case 5:
-            byteSize = serial.FIVEBITS
-        case 6:
-            byteSize = serial.SIXBITS
-        case 7:
-            byteSize = serial.SEVENBITS
-        case 8:
-            byteSize = serial.EIGHTBITS
-    return byteSize
+    def getSerialParity(self, parityInput):
+        parity = None
+        match parityInput:
+            case 'None':
+                parity = serial.PARITY_NONE
+            case 'Even':
+                parity = serial.PARITY_EVEN
+            case 'Odd':
+                parity = serial.PARITY_ODD
+            case 'Mark':
+                parity = serial.PARITY_MARK
+            case 'Space':
+                parity = serial.PARITY_SPACE
+        return parity
 
-def getSerialParity(parityInput):
-    parity = None
-    match parityInput:
-        case 'None':
-            parity = serial.PARITY_NONE
-        case 'Even':
-            parity = serial.PARITY_EVEN
-        case 'Odd':
-            parity = serial.PARITY_ODD
-        case 'Mark':
-            parity = serial.PARITY_MARK
-        case 'Space':
-            parity = serial.PARITY_SPACE
-    return parity
+    def getSerialStopBit(self, stopbitInput):
+        stopbit = None
+        match stopbitInput:
+            case 1:
+                stopbit = serial.STOPBITS_ONE
+            case 2:
+                stopbit = serial.STOPBITS_TWO
+        return stopbit
 
-def getSerialStopBit(stopbitInput):
-    stopbit = None
-    match stopbitInput:
-        case 1:
-            stopbit = serial.STOPBITS_ONE
-        case 2:
-            stopbit = serial.STOPBITS_TWO
-    return stopbit
+# Create a singleton instance
+serial_connection = SerialConnection()
